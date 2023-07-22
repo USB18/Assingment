57071ff1bc8da3a0a4a2a83683d4c9f620ed55d5
Umesh Singh Bisht <umeshsingh3199@gmai.com>
2023-07-22 22:40:39 +0530

    splash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { createRef } from 'react';
+import React, { createRef, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
 import MainStackNavigator from './src/navigator/stack-navigator';
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
   const navigationRef = createRef();
   return (
     <Provider store={store}>
